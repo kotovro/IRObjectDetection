141e83444f577cd2b5dd4e9cb1d777bdec92f388
Kotvoro <kotvoro@gmail.com>
2024-04-26 18:50:58 +0300

    Add object activity basic implewmnattion

--- app/src/main/java/com/example/scannerproto/AddObjectActivity.java
+++ app/src/main/java/com/example/scannerproto/AddObjectActivity.java
@@ -1,55 +1,69 @@
 package com.example.scannerproto;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import com.example.scannerproto.anlaysis.helpers.IObjectInfoAdder;
+import com.example.scannerproto.anlaysis.helpers.IObjectInfoGetter;
+import com.example.scannerproto.anlaysis.helpers.mockdb.SimpleObjectAdder;
+import com.example.scannerproto.anlaysis.helpers.mockdb.SimpleObjectInfoGetter;
+import com.example.scannerproto.anlaysis.helpers.mockdb.Thing;
 
 public class AddObjectActivity extends AppCompatActivity {
+
+    private IObjectInfoGetter infoGetter = new SimpleObjectInfoGetter();
+    private IObjectInfoAdder<Thing> adder = new SimpleObjectAdder();
     private EditText nameEditText;
     private EditText infoEditText;
+
+    private ITextValidator validator = new SimpleTextValidator();
+
     public void onReturn(View view) {
         finish();
     }
-    public  AddObjectActivity() {
 
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_addobject);
+
+        nameEditText = findViewById(R.id.nameEditText);
+        infoEditText = findViewById(R.id.objectInfo);
+
+        String receivedString = getIntent().getStringExtra("ObjectName");
+        if (receivedString != null) {
+            Toast.makeText(this, "Not enough info for object", Toast.LENGTH_SHORT).show();
+        }
     }
 
-//    @SuppressLint("MissingInflatedId")
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//            EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_addobject);
-//
-//        nameEditText = findViewById(R.id.nameEditText);
-//        infoEditText = findViewById(R.id.objectInfo);
-//
-//        String receivedString = getIntent().getStringExtra("ID");
-//        if (receivedString != null) {
-//            if (!MainActivity.newObjects.isEmpty()) {
-//                String obj = MainActivity.newObjects.get(0);
-//            }
-//            idEditText.setText(p.getPhoneNumber());
-//            LocalDate birthday = p.getBirthday();
-//            if (birthday != null) {
-//                ageEditText.setText(birthday.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-//            }
-//            nameEditText.setText(p.getName());
-//            String info = p.getInfo();
-//            if (!info.matches("\\s")) {
-//                infoEditText.setText(info);
-//            }
-//    }
-//
-//    Button selectDateButton = findViewById(R.id.selectDateButton);
-//        selectDateButton.setOnClickListener(v -> showDatePickerDialog(AddFriendActivity.this));
-//}
+    public void onAdd(View view){
+        String name = nameEditText.getText().toString();
+        String info = infoEditText.getText().toString();
+        if (validator.validate(info)) {
+            Thing thing = new Thing(name, info);
+            adder.addInfo(thing);
+            clearEditTexts();
+        } else {
+            Toast.makeText(this, "Not enough info for object", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void clearEditTexts() {
+        nameEditText.getText().clear();
+        infoEditText.getText().clear();
+    }
+
 }
---
+++ app/src/main/java/com/example/scannerproto/SimpleTextValidator.java
@@ -0,0 +1,9 @@
+package com.example.scannerproto;
+
+public class SimpleTextValidator implements ITextValidator {
+
+    @Override
+    public boolean validate(String s) {
+        return !s.isEmpty();
+    };
+}
